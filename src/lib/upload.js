3112b5e3e672b71848f7bf130b35206fa31b892c
techidsk <ecpknymt@gmail.com>
2023-04-27 20:15:04 +0800

    修改buffer

--- src/lib/upload.js
+++ src/lib/upload.js
@@ -14,7 +14,7 @@ async function downloadImage(url) {
     console.log('开始下载: ', url)
     try {
         const response = await fetch(url);
-        const buffer = await response.arrayBuffer();
+        const buffer = await response.buffer();
         return buffer;
     } catch (error) {
         console.error(`Error downloading image: ${error}`);
@@ -24,6 +24,7 @@ async function downloadImage(url) {
 async function uploadImage(url, object) {
     try {
         const buffer = await downloadImage(url)
+        console.log(typeof buffer)
         console.log('开始上传: ', url, object)
         await client.put(object, buffer);
     } catch (e) {
@@ -31,10 +32,10 @@ async function uploadImage(url, object) {
     }
 }
 
-export {
-    // put,
-    uploadImage
-}
+// export {
+//     // put,
+//     uploadImage
+// }
 
-// uploadImage('https://oaidalleapiprodscus.blob.core.windows.net/private/org-a8BzHSW1sg0jINwwAKFJMoLj/user-FrlBdFGPc8gQD7h904HEUfSa/img-xx6oD88dlVT5nnwyIWfaF7SV.png?st=2023-04-27T09%3A20%3A28Z&se=2023-04-27T11%3A20%3A28Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-04-27T09%3A58%3A07Z&ske=2023-04-28T09%3A58%3A07Z&sks=b&skv=2021-08-06&sig=8pd7B%2BedfTL6JavKbC2d26VQlnQZ83dRMmqL/9xj7zM%3D',
-//  '/project/_psy_/test/1.png')
+uploadImage('https://oaidalleapiprodscus.blob.core.windows.net/private/org-a8BzHSW1sg0jINwwAKFJMoLj/user-FrlBdFGPc8gQD7h904HEUfSa/img-twkzZzVb9uYSVK5aSqFfAsbI.png?st=2023-04-27T11%3A04%3A29Z&se=2023-04-27T13%3A04%3A29Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-04-27T11%3A44%3A51Z&ske=2023-04-28T11%3A44%3A51Z&sks=b&skv=2021-08-06&sig=alwHqJEKZVONavyN5OOIXKqWxNDs1DuIlyLX6cg6/Nk%3D',
+ '/project/_psy_/test/1.png')
