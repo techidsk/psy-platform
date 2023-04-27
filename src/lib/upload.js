9f7619a92946c6cabf6e24c125d261041e115162
techidsk <ecpknymt@gmail.com>
2023-04-27 20:16:13 +0800

    fixed: update error

--- src/lib/upload.js
+++ src/lib/upload.js
@@ -32,10 +32,7 @@ async function uploadImage(url, object) {
     }
 }
 
-// export {
-//     // put,
-//     uploadImage
-// }
-
-uploadImage('https://oaidalleapiprodscus.blob.core.windows.net/private/org-a8BzHSW1sg0jINwwAKFJMoLj/user-FrlBdFGPc8gQD7h904HEUfSa/img-twkzZzVb9uYSVK5aSqFfAsbI.png?st=2023-04-27T11%3A04%3A29Z&se=2023-04-27T13%3A04%3A29Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-04-27T11%3A44%3A51Z&ske=2023-04-28T11%3A44%3A51Z&sks=b&skv=2021-08-06&sig=alwHqJEKZVONavyN5OOIXKqWxNDs1DuIlyLX6cg6/Nk%3D',
- '/project/_psy_/test/1.png')
+export {
+    // put,
+    uploadImage
+}
