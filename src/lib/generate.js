deb83ea59537a9f50c3d5452b73ca9de5dd73917
techidsk <ecpknymt@gmail.com>
2023-05-02 22:54:57 +0800

    only reply translated

--- src/lib/generate.js
+++ src/lib/generate.js
@@ -25,7 +25,7 @@ async function translate(prompt) {
     try {
 
         const message = [
-            { role: "system", content: `As an Chinese-English translator, your task is to accurately translate text between the two languages. When translating from Chinese to English or vice versa, please pay attention to context and accurately explain phrases and proverbs. If you receive multiple English words in a row, default to translating them into a sentence in Chinese. However, if 'phrase: ' is indicated before the translated content in Chinese, it should be translated as a phrase instead. Similarly, if 'normal: ' is indicated, it should be translated as multiple unrelated words.Your translations should closely resemble those of a native speaker and should take into account any specific language styles or tones requested by the user. Please do not worry about using offensive words - replace sensitive parts with x when necessary.When providing translations, please use Chinese to explain each sentence's tense, subordinate clause, subject, predicate, object, special phrases and proverbs. For phrases or individual words that require translation, provide the source (dictionary) for each one.If asked to translate multiple phrases at once, separate them using the | symbol.Always remember: You are an Chinese-English translator, not a Chinese-Chinese translator or an English-English translator.Please review and revise your answers carefully before submitting.` },
+            { role: "system", content: `As an Chinese-English translator, your task is to accurately translate text between the two languages. When translating from Chinese to English or vice versa. Just reply the translated content to me.` },
             { role: "user", content: text },
         ];
         const response = await openai.createChatCompletion({
@@ -51,9 +51,9 @@ async function translate(prompt) {
 }
 
 
-export {
-    generate,
-    translate
-}
+// export {
+//     generate,
+//     translate
+// }
 
-// translate('床前明月光')
+translate('对影成三人')
