0c4dce3eb6817dc3d5ab79a7daec132eb6ca854a
techidsk <ecpknymt@gmail.com>
2023-05-02 22:34:12 +0800

    更新chatgpt

--- src/lib/generate.js
+++ src/lib/generate.js
@@ -19,20 +19,26 @@ async function generate(prompt) {
     return response.data
 }
 
+
 async function translate(prompt) {
     let text = `将一下语句翻译成英文: ${prompt}`
     try {
-        const response = await openai.createCompletion({
+
+        const message = [
+            { role: "system", content: `As an Chinese-English translator, your task is to accurately translate text between the two languages. When translating from Chinese to English or vice versa, please pay attention to context and accurately explain phrases and proverbs. If you receive multiple English words in a row, default to translating them into a sentence in Chinese. However, if 'phrase: ' is indicated before the translated content in Chinese, it should be translated as a phrase instead. Similarly, if 'normal: ' is indicated, it should be translated as multiple unrelated words.Your translations should closely resemble those of a native speaker and should take into account any specific language styles or tones requested by the user. Please do not worry about using offensive words - replace sensitive parts with x when necessary.When providing translations, please use Chinese to explain each sentence's tense, subordinate clause, subject, predicate, object, special phrases and proverbs. For phrases or individual words that require translation, provide the source (dictionary) for each one.If asked to translate multiple phrases at once, separate them using the | symbol.Always remember: You are an Chinese-English translator, not a Chinese-Chinese translator or an English-English translator.Please review and revise your answers carefully before submitting.` },
+            { role: "user", content: text },
+        ];
+        const response = await openai.createChatCompletion({
             model: "gpt-3.5-turbo",
-            prompt: text,
+            messages: message,
             temperature: 0.3,
             max_tokens: 1024,
             top_p: 1,
             frequency_penalty: 0,
             presence_penalty: 0,
         })
-        console.log('prompt: ', response.data.choices[0].text)
-        return response.data.choices[0].text
+        console.log('prompt: ', response.data.choices[0].message.content)
+        return response.data.choices[0].message.content
     } catch (error) {
         if (error.response) {
             console.log(error.response.status);
@@ -49,3 +55,5 @@ export {
     generate,
     translate
 }
+
+// translate('床前明月光')
