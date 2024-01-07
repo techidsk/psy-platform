3e7bdc0660cacc606984547df2c6fe7811bd6b06
techidsk <ecpknymt@gmail.com>
2024-01-07 21:47:31 +0800

    fix: bug

--- app/api/trail/route.ts
+++ app/api/trail/route.ts
@@ -5,41 +5,41 @@ import { getCurrentUser } from '@/lib/session';
 /**
  * /api/trail
  * 用户测试引擎效果
- * @returns 
+ * @returns
  */
 export async function POST(request: Request) {
-    const data = await request.json()
-    const currentUser = await getCurrentUser()
-    data['user_id'] = currentUser?.id
-    const experimentId = data['nano_id']
-    const experimentNanoId = data['experimentId'] || undefined
+    const data = await request.json();
+    const currentUser = await getCurrentUser();
+    data['user_id'] = currentUser?.id;
+    const experimentId = data['nano_id'];
+    const experimentNanoId = data['experimentId'] || undefined;
     // 插入用户实验表
     const dbExperiment = await db.user_experiments.findFirst({
         where: {
-            nano_id: experimentId
-        }
-    })
+            nano_id: experimentId,
+        },
+    });
     let d: any = {
         nano_id: experimentId,
         type: 'TRAIL',
         engine_id: parseInt(data['engine_id']),
-        user_id: data['user_id'],
-    }
+        user_id: parseInt(data['user_id']),
+    };
 
     if (experimentNanoId) {
         d = {
             ...d,
-            experiment_id: experimentNanoId
-        }
+            experiment_id: experimentNanoId,
+        };
     }
 
     if (!dbExperiment) {
         await db.user_experiments.create({
-            data: d
-        })
+            data: d,
+        });
     }
 
-    let trailNanoId = data['promptNanoId']
+    let trailNanoId = data['promptNanoId'];
     // 插入用户submit记录用以生成图片
     await db.trail.create({
         data: {
@@ -48,9 +48,9 @@ export async function POST(request: Request) {
             prompt: data['prompt'],
             engine_id: parseInt(data['engine_id']),
             state: 'GENERATING',
-            nano_id: trailNanoId
-        }
-    })
+            nano_id: trailNanoId,
+        },
+    });
 
-    return NextResponse.json({ 'msg': '发布成功' });
+    return NextResponse.json({ msg: '发布成功' });
 }
