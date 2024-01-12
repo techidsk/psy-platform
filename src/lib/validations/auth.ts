2ae223612e1fd315066f78e0f14e829b992c5d15
techidsk <ecpknymt@gmail.com>
2024-01-12 08:22:24 +0800

    update: modify password verify

--- src/lib/validations/auth.ts
+++ src/lib/validations/auth.ts
@@ -3,18 +3,38 @@ import * as z from 'zod';
 /**
  *  用户注册验证表单
  */
-export const registerSchema = z.object({
-    username: z.string().min(2, '用户名长度必须大于两位').max(100),
-    password: z.string().min(6, { message: '密码长度必须大于6位' }),
-    qualtrics: z.string().optional(),
-});
+export const registerSchema = z
+    .object({
+        username: z.string().min(2, '用户名长度必须大于两位').max(100),
+        password: z.string().min(8, { message: '密码长度必须大于6位' }),
+        qualtrics: z.string().optional(),
+    })
+    .superRefine(({ password }, checkPassComplexity) => {
+        const containsUppercase = (ch: string) => /[A-Z]/.test(ch);
+        const containsLowercase = (ch: string) => /[a-z]/.test(ch);
+        let countOfUpperCase = 0,
+            countOfLowerCase = 0,
+            countOfNumbers = 0;
+        for (let i = 0; i < password.length; i++) {
+            let ch = password.charAt(i);
+            if (!isNaN(+ch)) countOfNumbers++;
+            else if (containsUppercase(ch)) countOfUpperCase++;
+            else if (containsLowercase(ch)) countOfLowerCase++;
+        }
+        if (countOfLowerCase < 1 || countOfUpperCase < 1 || countOfNumbers < 1) {
+            checkPassComplexity.addIssue({
+                code: 'custom',
+                message: '密码必须包含至少一个大写字母，一个小写字母和一个数字',
+            });
+        }
+    });
 
 /**
  * 登录用验证表达
  */
 export const loginSchema = z.object({
     username: z.string().min(2, '用户名长度必须大于两位').max(100),
-    password: z.string().min(6, { message: '密码长度必须大于6位' }),
+    password: z.string().min(8, { message: '密码长度必须大于8位' }),
 });
 
 export const exprimentSchema = z.object({
@@ -26,15 +46,69 @@ export const exprimentSettingSchema = z.object({
     display_num: z.number().min(1).max(4, '不能超过4张'),
 });
 
-export const userFormSchema = z.object({
-    username: z.string().min(2, '用户名长度必须大于两位').max(100),
-    password: z.string().min(6, { message: '密码长度必须大于6位' }),
-    email: z.string().email({ message: '请输入合法的电子邮件地址' }).optional().or(z.literal('')),
-    tel: z.string().optional(),
-});
+export const userFormSchema = z
+    .object({
+        username: z.string().min(2, '用户名长度必须大于两位').max(100),
+        password: z.string().min(8, { message: '密码长度必须大于8位' }),
+        email: z
+            .string()
+            .email({ message: '请输入合法的电子邮件地址' })
+            .optional()
+            .or(z.literal('')),
+        tel: z.string().optional(),
+    })
+    .superRefine(({ password }, checkPassComplexity) => {
+        const containsUppercase = (ch: string) => /[A-Z]/.test(ch);
+        const containsLowercase = (ch: string) => /[a-z]/.test(ch);
+        let countOfUpperCase = 0,
+            countOfLowerCase = 0,
+            countOfNumbers = 0;
+        for (let i = 0; i < password.length; i++) {
+            let ch = password.charAt(i);
+            if (!isNaN(+ch)) countOfNumbers++;
+            else if (containsUppercase(ch)) countOfUpperCase++;
+            else if (containsLowercase(ch)) countOfLowerCase++;
+        }
+        if (countOfLowerCase < 1 || countOfUpperCase < 1 || countOfNumbers < 1) {
+            checkPassComplexity.addIssue({
+                code: 'custom',
+                message: '密码必须包含至少一个大写字母，一个小写字母和一个数字',
+            });
+        }
+    });
 
-export const userPatchFormSchema = z.object({
-    password: z.string().min(6, { message: '密码长度必须大于6位' }).optional().or(z.literal('')),
-    email: z.string().email({ message: '请输入合法的电子邮件地址' }).optional().or(z.literal('')),
-    tel: z.string().optional(),
-});
+export const userPatchFormSchema = z
+    .object({
+        password: z
+            .string()
+            .min(8, { message: '密码长度必须大于8位' })
+            .optional()
+            .or(z.literal('')),
+        email: z
+            .string()
+            .email({ message: '请输入合法的电子邮件地址' })
+            .optional()
+            .or(z.literal('')),
+        tel: z.string().optional(),
+    })
+    .superRefine(({ password }, checkPassComplexity) => {
+        if (!password) return;
+
+        const containsUppercase = (ch: string) => /[A-Z]/.test(ch);
+        const containsLowercase = (ch: string) => /[a-z]/.test(ch);
+        let countOfUpperCase = 0,
+            countOfLowerCase = 0,
+            countOfNumbers = 0;
+        for (let i = 0; i < password.length; i++) {
+            let ch = password.charAt(i);
+            if (!isNaN(+ch)) countOfNumbers++;
+            else if (containsUppercase(ch)) countOfUpperCase++;
+            else if (containsLowercase(ch)) countOfLowerCase++;
+        }
+        if (countOfLowerCase < 1 || countOfUpperCase < 1 || countOfNumbers < 1) {
+            checkPassComplexity.addIssue({
+                code: 'custom',
+                message: '密码必须包含至少一个大写字母，一个小写字母和一个数字',
+            });
+        }
+    });
