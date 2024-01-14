2cd0e74a43a2798a9f1b45fda6ed2d95630fbdac
techidsk <ecpknymt@gmail.com>
2024-01-14 19:35:45 +0800

    style: modify hero style

--- src/components/experiment/modules/centerd-hero.tsx
+++ src/components/experiment/modules/centerd-hero.tsx
@@ -1,33 +1,31 @@
-import { convertToParagraphs } from '@/lib/longtext'
+import { convertToParagraphs } from '@/lib/longtext';
 
 interface CenteredHeroProps extends React.HTMLAttributes<HTMLDivElement> {
-    title: string
-    content: string
-    buttonNum?: number
+    title: string;
+    content: string;
+    buttonNum?: number;
 }
 
-export function CenteredHero({
-    title,
-    content,
-    buttonNum = 0,
-    children
-}: CenteredHeroProps) {
+export function CenteredHero({ title, content, buttonNum = 0, children }: CenteredHeroProps) {
+    const paragraphs = convertToParagraphs(content);
 
-    const paragraphs = convertToParagraphs(content)
-
-    return <div className="hero">
-        <div className="hero-content text-center">
-            <div className="max-w-md">
-                <h1 className="text-5xl font-bold mb-12">{title}</h1>
-                <div className='flex flex-col gap-4'>
-                    {paragraphs.map((p, index) => (
-                        <p key={index} className='text-justify'>{p}</p>
-                    ))}
-                </div>
-                <div className={`flex ${buttonNum > 1 ? 'justify-between' : 'justify-center'}`}>
-                    {children}
+    return (
+        <div className="hero">
+            <div className="hero-content text-center">
+                <div className="max-w-md">
+                    <h1 className="text-5xl font-bold mb-8">{title}</h1>
+                    <div className="flex flex-col gap-4 mb-6">
+                        {paragraphs.map((p, index) => (
+                            <p key={index} className="text-justify">
+                                {p}
+                            </p>
+                        ))}
+                    </div>
+                    <div className={`flex ${buttonNum > 1 ? 'justify-between' : 'justify-center'}`}>
+                        {children}
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    );
 }
