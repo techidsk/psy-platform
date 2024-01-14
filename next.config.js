c98cbf48af8e66ac02b2f7965be6c1d4c2bc57fd
Wendaolee <leewendao@outlook.com>
2024-01-14 17:18:46 +0800

    Add CORS support for API Route
    这样子在可以在本地主机上不用做其他配置即可正常进行登录与注册流程。

--- next.config.js
+++ next.config.js
@@ -2,28 +2,47 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'zju-queen-psy.oss-cn-shanghai.aliyuncs.com',
-        port: '',
-      },
-      {
-        protocol: 'https',
-        hostname: 'techidsk.oss-cn-hangzhou.aliyuncs.com',
-        port: '',
-      },
-      {
-        protocol: 'https',
-        hostname: 'yuansun-psy.oss-cn-beijing.aliyuncs.com',
-        port: '',
-      },
-    ],
-  },
-  env: {
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL
-  }
-}
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'zju-queen-psy.oss-cn-shanghai.aliyuncs.com',
+                port: '',
+            },
+            {
+                protocol: 'https',
+                hostname: 'techidsk.oss-cn-hangzhou.aliyuncs.com',
+                port: '',
+            },
+            {
+                protocol: 'https',
+                hostname: 'yuansun-psy.oss-cn-beijing.aliyuncs.com',
+                port: '',
+            },
+        ],
+    },
+    env: {
+        NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    },
+    /**
+     * 为所有API路由响应头添加允许跨域头，以支持本地开发时远程调用开发机的API。
+     */
+    async headers() {
+        return [
+            {
+                source: '/api/:path*',
+                headers: [
+                    { key: 'Access-Control-Allow-Credentials', value: 'true' },
+                    { key: 'Access-Control-Allow-Origin', value: '*' },
+                    { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
+                    {
+                        key: 'Access-Control-Allow-Headers',
+                        value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+                    },
+                ],
+            },
+        ];
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
