3298d0fa243f716c2cd4a17c3bafe5751029cac0
techidsk <ecpknymt@gmail.com>
2024-01-15 23:08:56 +0800

    feat: add no data table

--- src/components/table/table.tsx
+++ src/components/table/table.tsx
@@ -15,13 +15,21 @@ export function Table({ datas, configs, children, searchNode }: TableProp) {
             {searchNode && <div className="table-header p-2">{searchNode}</div>}
             <table className="table w-full">
                 <TableHeader configs={configs} />
-                <tbody>
-                    {datas.map((data) => {
-                        return <TableRow key={data.id} data={data} configs={configs} />;
-                    })}
-                </tbody>
+                {datas.length > 0 && (
+                    <tbody>
+                        {datas.map((data) => {
+                            return <TableRow key={data.id} data={data} configs={configs} />;
+                        })}
+                    </tbody>
+                )}
             </table>
-            <div className="px-4 py-2 flex justify-end">{children}</div>
+            {datas.length === 0 ? (
+                <div className="w-full flex justify-center items-center h-[300px]">
+                    <div>暂无数据</div>
+                </div>
+            ) : (
+                <div className="px-4 py-2 flex justify-end">{children}</div>
+            )}
         </>
     );
 }
