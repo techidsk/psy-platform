81d54e15a2b8ebd11219d647525c82f3e59b2102
techidsk <ecpknymt@gmail.com>
2024-01-16 23:32:51 +0800

    feat: add table auth

--- src/components/table/table.tsx
+++ src/components/table/table.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { TableHeader } from './table-header';
 import { TableRow } from './table-row';
 import { TableConfig } from '@/types/table';
+import { getCurrentUser } from '@/lib/session';
+import { UserRole } from '@/types/user';
 
 interface TableProp {
     datas: any[];
@@ -9,16 +11,27 @@ interface TableProp {
     children?: React.ReactNode;
     searchNode?: React.ReactNode;
 }
-export function Table({ datas, configs, children, searchNode }: TableProp) {
+export async function Table({ datas, configs, children, searchNode }: TableProp) {
+    const currentUser = await getCurrentUser();
+    const role = currentUser?.role as UserRole;
+
+    // TODO 判断configs中的用户身份
+    let filterConfigs = configs;
+    if (role !== 'SUPERADMIN') {
+        filterConfigs = configs.filter((config) => {
+            return config.auth ? config.auth.includes(role) : true;
+        });
+    }
+
     return (
         <>
             {searchNode && <div className="table-header p-2">{searchNode}</div>}
             <table className="table w-full">
-                <TableHeader configs={configs} />
+                <TableHeader configs={filterConfigs} />
                 {datas.length > 0 && (
                     <tbody>
                         {datas.map((data) => {
-                            return <TableRow key={data.id} data={data} configs={configs} />;
+                            return <TableRow key={data.id} data={data} configs={filterConfigs} />;
                         })}
                     </tbody>
                 )}
--- src/types/table.d.ts
+++ src/types/table.d.ts
@@ -1,7 +1,10 @@
+import { UserRole } from './user';
+
 export type TableConfig = {
     key: string;
     label: string;
     className?: string;
     children: Function;
     hidden?: boolean;
+    auth?: UserRole[];
 };
