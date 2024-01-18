45ddf2dcf938789a74d618dd56f6d4a4165a19a6
techidsk <ecpknymt@gmail.com>
2024-01-18 23:50:26 +0800

    fix: nano id error

--- src/components/experiment/experiment-setting.tsx
+++ src/components/experiment/experiment-setting.tsx
@@ -59,7 +59,7 @@ export function ExperimentSetting() {
                                 图片显示数量
                             </label>
                             <input
-                                nano_id="display-num"
+                                data-name="display-num"
                                 placeholder="请输入图片显示数量"
                                 defaultValue={displayNum}
                                 type="number"
