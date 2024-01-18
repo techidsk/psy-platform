bf59059f547876d5a51501d4dcbaf8104f40c246
techidsk <ecpknymt@gmail.com>
2024-01-18 23:55:39 +0800

    fix

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -16,7 +16,7 @@ export function Toaster() {
 
     return (
         <ToastProvider>
-            {toasts.map(function ({ nano_id: id, title, description, action, ...props }) {
+            {toasts.map(function ({ id, title, description, action, ...props }) {
                 return (
                     <Toast key={id} {...props}>
                         <div className="grid gap-1">
