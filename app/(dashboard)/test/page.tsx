01f1a2cd161abbeb19f67f1ff131861c2de6d636
Wendaolee <leewendao@outlook.com>
2024-01-26 11:46:32 +0800

    feature: add test page

---
+++ app/(dashboard)/test/page.tsx
@@ -0,0 +1,10 @@
+export default async function TestPage() {
+    return (
+        <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+            <rect width="100%" height="100%" fill="#FF0000" />
+            <text x="50%" y="50%" fill="#FFFFFF" font-size="40" text-anchor="middle" dy=".3em">
+                A
+            </text>
+        </svg>
+    );
+}
