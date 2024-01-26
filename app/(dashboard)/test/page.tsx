85555b00de902f5940fce952e81d62f4cc585ba9
Wendaolee <leewendao@outlook.com>
2024-01-26 12:47:25 +0800

    feature: Add loading style

--- app/(dashboard)/test/page.tsx
+++ app/(dashboard)/test/page.tsx
@@ -1,10 +1,5 @@
+import { LoadingSpin } from '@/components/common/loading';
+
 export default async function TestPage() {
-    return (
-        <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-            <rect width="100%" height="100%" fill="#FF0000" />
-            <text x="50%" y="50%" fill="#FFFFFF" font-size="40" text-anchor="middle" dy=".3em">
-                A
-            </text>
-        </svg>
-    );
+    return <LoadingSpin></LoadingSpin>;
 }
