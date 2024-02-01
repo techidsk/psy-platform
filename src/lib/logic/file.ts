a01d5af3a9abc970f2eb2fc1130178fc8719e0e6
Wendaolee <leewendao@outlook.com>
2024-02-01 18:18:03 +0800

    feature: add api `/api/photo` `/api/psy`
    photo接口用于接收图片上传、检查数据库中是否存在该图片。
    psy接口则用于通过psy协议进行资源求取。

---
+++ src/lib/logic/file.ts
@@ -0,0 +1,72 @@
+/**
+ * 根据传入文件，根据SHA-256计算该文件的哈希值，返回该文件的哈希值字符串
+ * e.g input-file -> e3d6444b2c81165233ed8275a96d8670bb0a7997a9bfd4a7267037472dcdfbd8
+ * @param file
+ * @returns {string}
+ */
+export async function calculateHash(file: File): string {
+    const hashBuffer = await calculateFileHash(file);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashHex = hashArray.map((byte) => byte.toString(16).padStart(2, '0')).join('');
+    return hashHex;
+}
+
+function calculateFileHash(file: File) {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        const hashBuffer = new Uint8Array(32);
+
+        reader.onload = () => {
+            const data = new Uint8Array(reader.result);
+            crypto.subtle
+                .digest('SHA-256', data)
+                .then((hash) => {
+                    resolve(hash);
+                })
+                .catch((error) => {
+                    reject(error);
+                });
+        };
+
+        reader.onerror = () => {
+            reject(new Error('Failed to read the file.'));
+        };
+
+        reader.readAsArrayBuffer(file);
+    });
+}
+
+type CompressedStructure = {
+    compressedBlob: Blob;
+    fileOriginType: string;
+};
+
+/**
+ * 使用gzip方法压缩Blob对象。
+ * @param file
+ * @returns {CompressedStructure} 返回一个结构体，里面存放着已经丢失了原先文件类型信息的Blob对象和压缩前的Blob对象所指示的对象类型
+ */
+export async function compressBlob(file: Blob): CompressedStructure {
+    const compressedStream = file.stream().pipeThrough(new CompressionStream('gzip'));
+
+    const fileInfo = file.type;
+
+    const result = await new Response(compressedStream).blob();
+
+    console.log('the test compressed result:', result);
+
+    return {
+        compressedBlob: result,
+        fileOriginType: fileInfo,
+    };
+}
+
+async function readableToArray(reader) {
+    const chunks = [];
+    while (true) {
+        const { done, value } = await reader.read();
+        if (done) break;
+        chunks.push(value);
+    }
+    return Uint8Array.from(chunks.flat());
+}
---
+++ src/lib/pys-protocol.ts
@@ -0,0 +1,88 @@
+/**
+ * 提供对自定义的 psy:// 协议的解析。
+ * 基本格式为 psy://resoucename/querypath
+ *
+ * e.g psy://avatar/wendaolee
+ * e.g psy://photo/experiment/
+ * 该字符串主要用于实现对资源的定位获取，主要用于后端API处理。
+ *
+ * 由于该对象可能在客户端侧（浏览器环境下）使用，因此不适用使用URL对象进行解析——Browser和Nodejs环境中的二者行为不同。
+ */
+
+import { getUrl } from './url';
+
+export enum PsyResouceName {
+    Avatar = 'avatar',
+    Photo = 'photo',
+}
+
+export class PsyProtocol {
+    targetURLStr: string;
+    isLegeal: Boolean;
+    resouceName: string;
+    queryPathArr: Array<string>;
+    quertPath: string;
+
+    /**
+     * 传入形如`psy://resoucename/querypath`的字符串。
+     * @param pysUrl
+     */
+    constructor(pysUrl: string) {
+        this.targetURLStr = pysUrl;
+        this.isLegeal = /^psy:\/\/[^\/\s]+(?:\/[^\/\s]+)*$/.test(pysUrl);
+        if (this.isLegeal) {
+            const parser = (pysStr: string) => {
+                const resultArr = pysStr.slice(6).split('/');
+                const resouceName = resultArr.shift();
+
+                if (resultArr.length == 0) {
+                    return {
+                        resouceName: resouceName,
+                        resoucePath: null,
+                    };
+                }
+                return {
+                    resouceName: resouceName,
+                    queryPathArr: resultArr,
+                    queryPath: resultArr.join('/'),
+                };
+            };
+
+            const { resouceName, queryPathArr, queryPath } = parser(pysUrl);
+
+            this.resouceName = resouceName;
+            this.queryPathArr = queryPathArr;
+            this.quertPath = queryPath;
+            return this;
+        }
+    }
+
+    /**
+     * 获取请求对应资源的Promise对象
+     * @returns {Promise<Response>}
+     */
+    fetchResouce() {
+        return fetch(getUrl(`/api/psy?url=${this.targetURLStr}`));
+    }
+
+    /**
+     * 静态方法，用于直接通过传入资源名与查询路径获取符合psy协议规格的字符串。
+     * @param resouceName
+     * @param queryPathArr
+     */
+    static getProtocolString(resouceName: string, queryPathArr?: [string]) {
+        if (queryPathArr == undefined) {
+            return `psy://${resouceName}`;
+        }
+        return `psy://${resouceName}/${queryPathArr.join('/')}`;
+    }
+
+    /**
+     * 静态方法，用于传入psy协议字符串判断其是否合法
+     * @param str
+     * @returns {boolean}
+     */
+    static isStringLegal(str: string) {
+        return /^psy:\/\/[^\/\s]+(?:\/[^\/\s]+)*$/.test(str);
+    }
+}
