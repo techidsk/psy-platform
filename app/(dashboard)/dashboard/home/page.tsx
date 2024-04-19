8377fb582935a00b6700dc8d13c7252cb7031927
techidsk <ecpknymt@gmail.com>
2024-04-19 20:24:51 +0800

    fix: admin user dashboard experiment

--- app/(dashboard)/dashboard/home/page.tsx
+++ app/(dashboard)/dashboard/home/page.tsx
@@ -1,7 +1,9 @@
-import './styles.css';
 import { DashboardHeader } from '@/components/dashboard-header';
 import { getCurrentUser } from '@/lib/session';
 import { redirect } from 'next/navigation';
+import { findLastExperiment, getUserGroupExperiments } from '@/lib/user_experiment';
+import ExperimentTimeline from '@/components/experiment/timeline/experiment-timeline';
+import { logger } from '@/lib/logger';
 
 // 获取用户在当前项目中的分组实验内容
 // 1. 如果用户从未登陆，则查看是否当前项目，否则显示当前时间尚未开放实验
@@ -18,15 +20,41 @@ export default async function Dashboard() {
     const needExperiment = currentUser.role === 'USER';
 
     // TODO 查看用户下次实验记录时间以及是否需要开始下次实验
-    if (needExperiment) {
-        redirect('/dashboard/home');
+    if (!needExperiment) {
+        redirect('/dashboard');
     }
+
+    const { experiment_id: nextExperimentId, project_group_id: projectGroupId } =
+        await getUserGroupExperiments();
+
+    if (!nextExperimentId) {
+        redirect('/closed');
+    }
+    if (!projectGroupId) {
+        redirect('/closed');
+    }
+
+    const result = await findLastExperiment(currentUser.id, projectGroupId);
+    const needWait = result?.status || false;
+    const timeStamp = result.timeStamp;
+    logger.info(`<用户${currentUser.id}> 需要等待 ${timeStamp} 进行实验 ${nextExperimentId}`);
+    if (needWait) {
+        redirect(`/dashboard/wait?t=${timeStamp}`);
+    }
+    logger.info(`<用户${currentUser.id}> 需要进行实验 ${nextExperimentId}`);
 
     return (
         <>
             <div className="container mx-auto">
                 <div className="flex flex-col gap-4">
                     <DashboardHeader heading="控制台" text="用户相关操作页面" />
+                    {needExperiment && (
+                        <ExperimentTimeline
+                            nextExperimentId={nextExperimentId}
+                            userId={parseInt(currentUser.id)}
+                            guest={false}
+                        />
+                    )}
                 </div>
             </div>
         </>
