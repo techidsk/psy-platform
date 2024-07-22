5bf048b493365fafee9baebedde628db736bd891
techidsk <ecpknymt@gmail.com>
2024-07-22 12:05:37 +0800

    edit ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,17 @@
 // ecosystem.config.js
-module.exports = { apps: [{ name: 'psy', script: 'pnpm', args: 'run start', env: {
-      // 设置环境变量
-      NODE_ENV: 'production'
-    }
-  }]
+module.exports = {
+    apps: [
+        {
+            name: 'psy',
+            script: 'pnpm',
+            args: 'run start',
+            env: {
+                // 设置环境变量
+                NODE_ENV: 'production',
+            },
+            autorestart: true,
+            log_date_format: 'YYYY-MM-DD HH:mm:ss',
+            cwd: '/root/code/psy-platform',
+        },
+    ],
 };
