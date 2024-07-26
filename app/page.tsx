8c06519cefcba2eef63ea1b459177c25a3f410f5
techidsk <ecpknymt@gmail.com>
2024-07-26 20:34:13 +0800

    feat: add footer

--- app/page.tsx
+++ app/page.tsx
@@ -7,28 +7,33 @@ export default async function Home() {
     return (
         <div className="h-screen">
             <div className="login-left w-96 hidden md:block" />
-            <div className="bg-white login-right ml-0 md:ml-96 ">
-                <div className="flex flex-col gap-4 justify-center items-center h-screen">
-                    <h1 className="font-bold text-gray-800 text-3xl mb-8">欢迎</h1>
-                    {currentUser?.id ? (
-                        <div className="flex flex-col gap-4 justify-center items-center">
-                            <Link href={'/dashboard'} className="btn btn-primary w-[360px]">
-                                返回控制台
-                            </Link>
-                        </div>
-                    ) : (
-                        <div className="flex flex-col gap-4 justify-center items-center">
-                            <Link href={'/login'} className="btn btn-primary w-[360px]">
-                                登录
-                            </Link>
-                            <Link
-                                href={'/register'}
-                                className="btn btn-ghost btn-outline w-[360px]"
-                            >
-                                注册
-                            </Link>
-                        </div>
-                    )}
+            <div className="bg-white login-right ml-0 md:ml-96">
+                <div className="h-full flex flex-col justify-between">
+                    <div className="flex flex-1 flex-col gap-4 justify-center items-center">
+                        <h1 className="font-bold text-gray-800 text-3xl mb-8">欢迎</h1>
+                        {currentUser?.id ? (
+                            <div className="flex flex-col gap-4 justify-center items-center">
+                                <Link href={'/dashboard'} className="btn btn-primary w-[360px]">
+                                    返回控制台
+                                </Link>
+                            </div>
+                        ) : (
+                            <div className="flex flex-col gap-4 justify-center items-center">
+                                <Link href={'/login'} className="btn btn-primary w-[360px]">
+                                    登录
+                                </Link>
+                                <Link
+                                    href={'/register'}
+                                    className="btn btn-ghost btn-outline w-[360px]"
+                                >
+                                    注册
+                                </Link>
+                            </div>
+                        )}
+                    </div>
+                    <footer className="flex-0">
+                        <div className="flex justify-center my-4">This is footer</div>
+                    </footer>
                 </div>
             </div>
         </div>
